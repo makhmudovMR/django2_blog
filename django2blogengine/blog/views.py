cb6515279abd93555add26913cfdc8a6d79f2034
makhmudovMR <mmriadovich@gmail.com>
2019-04-25 16:43:34 +0300

    model 1

--- django2blogengine/blog/views.py
+++ django2blogengine/blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post
 
 def posts_list(request):
+    posts = Post.objects.all()
     context = {
-        'name': 'Magomed',
-        'names': ['John', 'Boris', 'Oleg']
+        'posts': posts,
         }
     return render(request, 'blog/index.html', context=context)
+
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    context={'post':post}
+    return render(request, 'blog/post_detail.html', context=context)
